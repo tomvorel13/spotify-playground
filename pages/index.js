916993a05d506fd73f9bc129e7d688e7f241df8d
Tom Vorel <tomvorel13@gmail.com>
2021-03-27 20:22:01 +0100

    Deleting vercel stuff

--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,18 @@ import Head from 'next/head'
 import useSWR from 'swr'
 
 import fetcher from '../lib/fetcher'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   const { data } = useSWR('/api/top-tracks', fetcher)
 
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Spotify Playground</title>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <main>
+        <h1>Hi there!</h1>
       </main>
     </div>
   )
