df6ba0e29ad8f41066bbeaf74768e3499b328c49
Tom Vorel <tomvorel13@gmail.com>
2021-03-28 12:24:54 +0200

    Current track check for 500s or 204s

--- pages/api/v1/spotify/current-track.js
+++ pages/api/v1/spotify/current-track.js
@@ -2,6 +2,11 @@ import { getCurrentTrack } from '@/lib/spotify'
 
 export default async (_, res) => {
   const response = await getCurrentTrack()
+
+  if (response.status === 204 || response.status > 400) {
+    return res.status(200).json({ isPlaying: false })
+  }
+
   const data = await response.json()
 
   return res.status(200).json(data)
